c167adc186943440500bcb9ab19598216678c5d2
brian <omondibrian392@gmail.com>
2021-08-08 12:34:19 +0300

    changed socket.io client

--- lib/app/modules/chat/controllers/chat_controller.dart
+++ lib/app/modules/chat/controllers/chat_controller.dart
@@ -3,16 +3,19 @@ import 'dart:io';
 import 'package:get/get.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:image_picker/image_picker.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:adhara_socket_io/adhara_socket_io.dart';
 
 import '../model/chat.model.dart';
 import '../providers/chat_provider.dart';
 
 class ChatController extends GetxController {
-  late IO.Socket _io;
+  late SocketIOManager _io;
+
   ChatProvider _provider = ChatProvider();
   var imagePicker = ImagePicker();
   File? image;
-  
+
   RxList<ChatModel> currentChats = RxList<ChatModel>();
   var userName = ''.obs;
   var typingUser = ''.obs;
@@ -21,7 +24,9 @@ class ChatController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    _io = IO.io('http://147.182.196.55:8000', <String, dynamic>{
+    _io = SocketIOManager();
+
+    IO.io('http://147.182.196.55:8000', <String, dynamic>{
       "transports": ["websocket"],
       "autoConnect": false,
     });
@@ -35,7 +40,6 @@ class ChatController extends GetxController {
 
   @override
   void onClose() {
-    _io.close();
   }
 
   //add new user to the collective
@@ -44,17 +48,18 @@ class ChatController extends GetxController {
     update();
   }
 
-  void connect() {
-    _io.connect();
-    _io.onConnect((_) {
+  void connect() async {
+    SocketIO socket = await getSocket();
+
+    socket.onConnect.listen((_) {
       print('connect');
     });
     //notify other users of your status
-    _io.emit(SocketEvents.USER, userName.value);
+    socket.emit(SocketEvents.USER, [userName.value]);
     //request for the current available chats
-    _io.emit(SocketEvents.FETCH_CHATS, null);
+    socket.emit(SocketEvents.FETCH_CHATS, []);
     //listen for incomming chats
-    _io.on(SocketEvents.CHATS, (chats) {
+    socket.on(SocketEvents.CHATS).listen((chats) {
       print(chats.toString());
       for (var i = 0; i <= chats.length; i++) {
         final chat = chats[i];
@@ -64,29 +69,44 @@ class ChatController extends GetxController {
       update();
     });
     //check for online users and notify the user
-    _io.on(SocketEvents.online, (user) {
+    socket.on(SocketEvents.online).listen((user) {
       Get.snackbar('testing', '$user is online');
       print('$user is online');
     });
     //listen for typing events
-    _io.on(SocketEvents.typing, (user) {
+    socket.on(SocketEvents.typing).listen((user) {
       typingUser.value = user;
       update();
     });
     //listen for incoming chat
-    _io.on(SocketEvents.CHAT, (chat) {
+    socket.on(SocketEvents.CHAT).listen((chat) {
       currentChats.add(chat);
       print(chat);
       update();
     });
   }
 
-  void sendChat(ChatModel chat) async {
-    var result = await _provider.sendChat(chat, userName.value);
-    _io.emit(SocketEvents.newChat, result.toJson());
+  Future<SocketIO> getSocket() async {
+    final socket = await _io.createInstance(SocketOptions(
+      //Socket IO server URI
+      'http://147.182.196.55:8000',
+      //Enable or disable platform channel logging
+      enableLogging: true,
+      transports: [
+        Transports.webSocket,
+        // Transports.polling,
+      ], //Enable required transport
+    ));
+    return socket;
   }
 
-    Future getImage() async {
+  void sendChat(ChatModel chat) async {
+    var result = await _provider.sendChat(chat, userName.value);
+    final socket = await getSocket();
+    socket.emit(SocketEvents.newChat, [result.toJson()]);
+  }
+
+  Future getImage() async {
     XFile? pickedFile;
     pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
@@ -94,5 +114,4 @@ class ChatController extends GetxController {
 
     }
   }
-
 }
